0ce1160d80609869b5fff715dae1336bb5a92359
Bossa Web Solutions <gvfbossa@hotmail.com>
2024-10-31 21:16:20 -0300

    Implemented prod endpoint and method

--- src/main/java/br/com/bossawebsolutions/simpleproductsregister/config/CorsConfig.java
+++ src/main/java/br/com/bossawebsolutions/simpleproductsregister/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost:4200", "https://simple-products-register-backend.onrender.com")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
